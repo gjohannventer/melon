c0e03312872b55f853d27369064a2f074961320f
aonixe <g.johannventer@gmail.com>
2020-01-29 21:44:33 +0200

    add missing files

---
+++ ClinicManagement/Controllers/Api/AppointmentController.cs
@@ -0,0 +1,38 @@
+﻿using ClinicManagement.Core;
+using ClinicManagement.Core.Dto;
+using ClinicManagement.Core.Models;
+using System.Web.Http;
+
+namespace ClinicManagement.Controllers.Api
+{
+    public class AppointmentController : ApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public AppointmentController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateAppointment(AppointmentCreateDto appointCreateDto)
+        {
+            var appointment = new Appointment
+            {
+                Detail = appointCreateDto.Detail,
+                DoctorId = appointCreateDto.DoctorId,
+                PatientId = appointCreateDto.PatientId,
+                StartDateTime = appointCreateDto.StartDateTime,
+                Status = appointCreateDto.Status
+            };
+
+            //Check if the slot is available
+            if (_unitOfWork.Appointments.ValidateAppointment(appointment.StartDateTime, appointCreateDto.DoctorId))
+                return NotFound();// Should return a better error with a message but it takes a bit of time to implement
+
+            _unitOfWork.Appointments.Add(appointment);
+            _unitOfWork.Complete();
+
+            return Ok();
+        }
+    }
+}
---
+++ ClinicManagement/Controllers/Api/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using ClinicManagement.Core;
+using System.Linq;
+using System.Web.Http;
+
+namespace ClinicManagement.Controllers.Api
+{
+    public class UsersController : ApiController
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public UsersController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IHttpActionResult GetPatients()
+        {
+            return  Ok(_unitOfWork.Users.GetUsers().Where(user => user.IsActive.Value)); // should it return a viewModel?
+        }
+    }
+}
